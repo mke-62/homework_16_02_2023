2ffa1dcecf399db8b31a265150612308be0a52b5
Konstantin <mke-62@ya.ru>
2023-02-25 22:00:04 +0300

    1dep
    HW

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import Dost from "./pages/Dost";
+import Dost2 from "./pages/Dost2";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+
+        <div className="Logo"><img  src="https://admrzn.ru/tpl/img/logo.png" alt=""/> <h2>Рязань</h2></div>
+        <BrowserRouter>
+            <div className="menu">
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/dost">Памятник Олегу Рязанскому
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/dost2">Успенский собор
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/dost" element={<Dost />} />
+              <Route path="/dost2" element={<Dost2 />} />
+
+          </Routes>
+        </BrowserRouter>
+      </div>
   );
 }
-
-export default App;
